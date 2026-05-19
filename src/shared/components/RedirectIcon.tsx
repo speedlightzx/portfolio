2f554b6f529257d08b970933c97081dbd1a33f4d
Allan <speedlightbr@gmail.com>
2026-05-19 16:53:54 +0000

    feat: adiciona pagina de contato

--- src/shared/components/RedirectIcon.tsx
+++ src/shared/components/RedirectIcon.tsx
@@ -1,14 +1,23 @@
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/shared/components/ui/tooltip";
+import type { RedirectIcon } from "../types/iRedirectIcon";
 
 export function RedirectIcon(
-    { imagePath, url, alt, toolTipMessage }: { imagePath:string, url:string, alt:string, toolTipMessage?:string }
+    { 
+        alt, 
+        imagePath, 
+        url,
+        imageSize, 
+        toolTipMessage 
+    }: RedirectIcon
 ) {
+
+    const size = imageSize ? `w-${imageSize} h-${imageSize}` : 'w-8 h-8'
     return (
         <TooltipProvider>
             <Tooltip>
                 <TooltipTrigger asChild>
                     <img 
-                    className="invert w-8 h-8 hover:cursor-pointer transform duration-200 hover:scale-110" 
+                    className={`invert w-10 h-10 hover:cursor-pointer transform duration-200 hover:scale-110`} 
                     src={imagePath} 
                     alt={alt} 
                     onClick={() => window.open(url, "_blank", "noopener,noreferrer")}/>
---
+++ src/shared/types/iRedirectIcon.tsx
@@ -0,0 +1,7 @@
+export interface RedirectIcon {
+    imagePath:string, 
+    url:string, 
+    alt:string, 
+    toolTipMessage?:string
+    imageSize?:number
+}
